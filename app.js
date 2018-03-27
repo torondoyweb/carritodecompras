f248267fb5fe19b7308c2f38d9676ec2607f1a42
torondoyweb <torondoyweb@gmail.com>
2018-03-27 08:16:29 -0400

    la primera linea de app.js tenia mal escrito la palabra var

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-ar express = require('express');
+var express = require('express');
 var path = require('path')
 var favicon = require('serve-favicon')
 var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index = require('./routes/index')
+var users = require('./routes/users')
 
-var app = express();
+var app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
